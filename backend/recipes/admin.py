989422298acbacac8712e58e9b09507db27d09b9
Igor Mogilin <endless8space@gmail.com>
2025-05-16 17:40:04 +0300

    Fix recipies models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from recipes.models import Recipe
+from recipes.models import Recipe, IngredientInRecipe
 
+
+class IngredientInRecipeInline(admin.TabularInline):
+    model = IngredientInRecipe
+    extra = 1
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'favorites_count')
     search_fields = ('name', 'author__username')
     list_filter = ('tags',)
+    inlines = [IngredientInRecipeInline]
 
     def favorites_count(self, obj):
         """Получение количества добавлений в избранное."""
