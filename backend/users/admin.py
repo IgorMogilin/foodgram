292cea7be9cfaabeb6cec3d3504d4a4c46119deb
Igor Mogilin <endless8space@gmail.com>
2025-05-17 16:14:15 +0300

    fix admin users

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,21 +1,39 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
 
 
+class RequiredFieldsUserCreationForm(UserCreationForm):
+    """
+    Форма создания пользователя с обязательными:
+    - email
+    - first_name
+    - last_name
+    """
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    add_form = RequiredFieldsUserCreationForm
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('username', 'email')
-    list_filter = ('is_staff', 'is_superuser')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': (
-            'first_name',
-            'last_name',
-            'email',
-            'avatar'
+        ('Персональная информация', {
+            'fields': ('first_name', 'last_name', 'email', 'avatar')
+        }),
+        ('Права доступа', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser'
         )}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
